c1b27f9b8b9e6e1d452d90637188a32322d8411e
bachie07 <giabach0704@gmail.com>
2026-02-07 14:58:40 +1030

    create middleware protect file to verify token

---
+++ src/middleware/authmiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+
+        return res.status(401).json({error: "Access denided. No token provided!"})
+    }
+
+    try{
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = verified;
+
+        next();
+
+    }
+    catch(error){
+        res.status(401).json({ error: "invalid token"});
+    }
+
+};
+

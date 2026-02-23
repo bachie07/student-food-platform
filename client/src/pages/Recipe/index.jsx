a612ce319f1cac7fb9e6b511df7be78542df3eb2
bachie07 <giabach0704@gmail.com>
2026-02-23 14:48:52 +1030

    feat: create recipe page to display recipes

--- client/src/pages/Recipe/index.jsx
+++ client/src/pages/Recipe/index.jsx
@@ -1,10 +1,61 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const RecipePage = () => {
 
+  const [recipes, setRecipes] = useState([])
 
-    return (
-    <h1> About Page</h1>
+  const API_URL = 'http://localhost:5001/api/recipe/getrecipe'
+
+  useEffect(() => {
+
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setRecipes(data.recipes))
+    }, [])
+  
+    console.log("Recipes returned", recipes)
+
+
+  return (
+    
+    <div className="min-h-screen bg-gray-50">
+
+    <section className=" flex justify-center"> 
+
+    <div className="text-center mt-10"> 
+
+    <h1 className="font-bold text-black text-[30px] font-serif mt-10"> Popular Recipes</h1>
+
+    <h3 className=" text-gray-500"> {recipes.length} recipes</h3>
+
+    </div>
+
+    </section>
+
+    <section className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-20 px-10">
+
+      { recipes.map(recipe => (
+
+        <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
+        <div className="block p-25 w-full bg-transparent rounded-lg border border-none mb-20" > 
+
+          <img src={recipe.imageUrl} alt={recipe.title} className="w-full h-full object-cover rounded-md mb-4" />
+
+          <h1 className="mb-2 text-2xl font-serif font-bold text-gray-900"> {recipe.title}</h1>
+
+          
+
+        </div>
+        </Link>
+
+      )) }
+
+    </section>
+
+    </div>
+
     
     )
 

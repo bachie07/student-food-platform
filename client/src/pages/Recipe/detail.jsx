013396c90a1664c440bb66182d26adc134bb21ed
bachie07 <giabach0704@gmail.com>
2026-02-26 23:57:15 +1030

    update api endpoint recipe detail final

--- client/src/pages/Recipe/detail.jsx
+++ client/src/pages/Recipe/detail.jsx
@@ -11,7 +11,7 @@ const RecipeDetail = () => {
 
     useEffect(() => { // hook to fetch backend using id 
 
-        fetch(`${import.meta.env.VITE_API_URL}/${id}`)
+        fetch(`${import.meta.env.VITE_API_URL}/recipe/getrecipe/${id}`)
         .then(res => res.json())
         .then(data => setRecipe(data))
         .catch(err => {console.error(err); setError(err);})
